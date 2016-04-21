4bbc66c7a92507495a82242a46eb6b60024df21a
Lukasz Krefft <lukasz@snowy.pl>
2016-04-22 01:25:56 +0200

    First commit

---
+++ jsoner/src/main/java/pl/snowy/Book.java
@@ -0,0 +1,25 @@
+package pl.snowy;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlRootElement
+public class Book {
+
+	private String title;
+
+	private String isbn;
+
+	public Book(String title, String isbn) {
+		super();
+		this.title = title;
+		this.isbn = isbn;
+	}
+
+	public Book() {
+		super();
+	}
+
+}
---
+++ jsoner/src/main/java/pl/snowy/BooksResource.java
@@ -0,0 +1,22 @@
+package pl.snowy;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+
+@Path("book")
+public class BooksResource {
+
+	@GET
+	public String getBook() {
+		return "Ptaki polskie";
+	}
+
+	@GET
+	@Produces("application/json")
+	@Path("isbn={isbn}")
+	public Book getBook(@PathParam("isbn") String isbn) {
+		return new Book("Cool book", isbn );
+	}
+}
