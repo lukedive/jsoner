7c246121141c34b29ec8cd8e4a3243716a108f6f
Lukasz Krefft <lukasz@snowy.pl>
2016-05-01 20:40:26 +0200

    # WARNING: head commit changed in the meantime
    Merge branch 'testbranch2'
    
    Conflicts:
    	git-playground/src/pl/snowy/playground/git/GitApp.java

--- git-playground/src/pl/snowy/playground/git/GitApp.java
+++ git-playground/src/pl/snowy/playground/git/GitApp.java
@@ -5,6 +5,7 @@ public class GitApp {
 	public static void main(String[] args) {
 		System.out.println("Hello git!!!");
 		System.out.println("New output on testbranch1+2");
+		System.out.println("Something new");
 	}
 
 }
